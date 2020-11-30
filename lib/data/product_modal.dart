504954ae1adf9ae1007af584940e7e658bf653c7
RohitEdathil <44530239+RohitEdathil@users.noreply.github.com>
2020-11-30 23:12:30 +0530

    Added product creation, still needs testing

---
+++ lib/data/product_modal.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<String> createProduct(String name, String description, String measure,
+    String sellingPrice) async {
+  String response;
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  String uid = pref.getString('uid');
+  await FirebaseFirestore.instance
+      .collection('products')
+      .add({
+        "owner": uid,
+        "productName": name,
+        "description": description,
+        "measure": measure,
+        "sellingPrice": double.parse(sellingPrice)
+      })
+      .then((value) => {response = 'OK'})
+      .catchError((error) {
+        response = "Network Error";
+        print(error);
+      });
+  return response;
+}
--- lib/views/product_add.dart
+++ lib/views/product_add.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ishop/data/product_modal.dart';
 
 class ProductAdd extends StatelessWidget {
   @override
@@ -21,7 +22,7 @@ class ProductAdd extends StatelessWidget {
         ),
         elevation: 0,
       ),
-      body: ProductForm(),
+      body: SingleChildScrollView(child: ProductForm()),
     );
   }
 }
@@ -33,63 +34,101 @@ class ProductForm extends StatefulWidget {
 
 class _ProductFormState extends State<ProductForm> {
   final _formKey = GlobalKey<FormState>();
+  final List textFieldsValue = [];
+  String measureChoice;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(30),
       child: Form(
-        child: Column(
-          children: [
-            ProductFormField(
-              placeholder: 'Product Name',
-            ),
-            ProductFormField(
-              placeholder: 'Description',
-            ),
-            DropdownButtonFormField(
-              items: [
-                DropdownMenuItem(
-                  child: Text('Weight/Kg'),
-                  value: 'kg',
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              ProductFormField(
+                placeholder: 'Product Name',
+                dataList: textFieldsValue,
+              ),
+              ProductFormField(
+                placeholder: 'Description',
+                dataList: textFieldsValue,
+              ),
+              DropdownButtonFormField(
+                items: [
+                  DropdownMenuItem(
+                    child: Text('Weight/Kg'),
+                    value: 'kg',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Weight/gram'),
+                    value: 'g',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Number'),
+                    value: 'no',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Litre'),
+                    value: 'L',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('Metre'),
+                    value: 'm',
+                  ),
+                ],
+                onChanged: (value) {
+                  measureChoice = value;
+                },
+                style: TextStyle(color: Theme.of(context).backgroundColor),
+                dropdownColor: Theme.of(context).accentColor,
+                iconEnabledColor: Colors.black,
+                decoration: InputDecoration(
+                    hintStyle:
+                        TextStyle(color: Theme.of(context).backgroundColor),
+                    hintText: 'Measured by',
+                    focusedBorder: UnderlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: BorderSide(
+                        color: Theme.of(context).backgroundColor,
+                        width: 5,
+                      ),
+                    )),
+              ),
+              ProductFormField(
+                placeholder: 'Selling Price Per Unit',
+                dataList: textFieldsValue,
+                type: TextInputType.number,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: OutlineButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      createProduct(textFieldsValue[0], textFieldsValue[1],
+                              measureChoice, textFieldsValue[2])
+                          .then((value) {
+                        if (value == 'OK') {
+                          Navigator.pop(context);
+                        } else {
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                            content: Text(value),
+                            backgroundColor: Colors.red,
+                          ));
+                        }
+                      });
+                    }
+                    textFieldsValue.clear();
+                  },
+                  child: Text(
+                    'Create',
+                    style: TextStyle(color: Theme.of(context).backgroundColor),
+                  ),
+                  borderSide:
+                      BorderSide(color: Theme.of(context).backgroundColor),
                 ),
-                DropdownMenuItem(
-                  child: Text('Weight/gram'),
-                  value: 'g',
-                ),
-                DropdownMenuItem(
-                  child: Text('Number'),
-                  value: 'no',
-                ),
-                DropdownMenuItem(
-                  child: Text('Litre'),
-                  value: 'L',
-                ),
-                DropdownMenuItem(
-                  child: Text('Metre'),
-                  value: 'm',
-                ),
-              ],
-              onChanged: (value) {},
-              style: TextStyle(color: Theme.of(context).backgroundColor),
-              dropdownColor: Theme.of(context).accentColor,
-              iconEnabledColor: Colors.black,
-              decoration: InputDecoration(
-                  hintStyle:
-                      TextStyle(color: Theme.of(context).backgroundColor),
-                  hintText: 'Measured by',
-                  focusedBorder: UnderlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    borderSide: BorderSide(
-                      color: Theme.of(context).backgroundColor,
-                      width: 5,
-                    ),
-                  )),
-            ),
-            ProductFormField(
-              placeholder: 'Selling Price Per Unit',
-              type: TextInputType.number,
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -99,7 +138,8 @@ class _ProductFormState extends State<ProductForm> {
 class ProductFormField extends StatefulWidget {
   final String placeholder;
   final TextInputType type;
-  ProductFormField({this.placeholder, this.type});
+  final List dataList;
+  ProductFormField({this.placeholder, this.type, this.dataList});
   @override
   _ProductFormFieldState createState() => _ProductFormFieldState();
 }
@@ -107,19 +147,30 @@ class ProductFormField extends StatefulWidget {
 class _ProductFormFieldState extends State<ProductFormField> {
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      style: TextStyle(color: Theme.of(context).backgroundColor),
-      keyboardType: widget.type == null ? TextInputType.text : widget.type,
-      decoration: InputDecoration(
-          hintText: widget.placeholder,
-          hintStyle: TextStyle(color: Theme.of(context).backgroundColor),
-          focusedBorder: UnderlineInputBorder(
-            borderRadius: BorderRadius.circular(50),
-            borderSide: BorderSide(
-              color: Theme.of(context).backgroundColor,
-              width: 5,
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      child: TextFormField(
+        validator: (input) {
+          if (input.isEmpty) {
+            return 'Field must not be empty';
+          }
+          widget.dataList.add(input);
+          return null;
+        },
+        style: TextStyle(color: Theme.of(context).backgroundColor),
+        keyboardType: widget.type == null ? TextInputType.text : widget.type,
+        decoration: InputDecoration(
+            hintText: widget.placeholder,
+            hintStyle: TextStyle(color: Theme.of(context).backgroundColor),
+            focusedBorder: UnderlineInputBorder(
+              borderRadius: BorderRadius.circular(50),
+              borderSide: BorderSide(
+                color: Theme.of(context).backgroundColor,
+                width: 5,
+              ),
             ),
-          )),
+            errorStyle: TextStyle(color: Theme.of(context).backgroundColor)),
+      ),
     );
   }
 }
